25509d014f8ec2ba9e0a8a97978c94f1d29d7e6f
Karim Assifar <82780260+Assifar-Karim@users.noreply.github.com>
2023-01-10 17:50:10 +0100

    Setup Spring Boot App

---
+++ Backend/src/main/java/com/monasCI/MonasCiApplication.java
@@ -0,0 +1,13 @@
+package com.monasCI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonasCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonasCiApplication.class, args);
+	}
+
+}
